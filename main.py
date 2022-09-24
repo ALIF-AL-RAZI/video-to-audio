1dbbce8cf4bf97d49fd79365b58a963cca24d27f
ALIF-AL-RAZI <alif.razi@northsouth.edu>
2022-09-24 16:01:52 +0600

    gui implement

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from moviepy.editor import *
+from tkinter import *
+from tkinter import filedialog
+import os
 
-clip = VideoFileClip("videoplayback.mp4")
+main = Tk()
+main.geometry("630x500+400+100")
+main.title("video2audio")
+main.configure(bg="#00ffff")
 
-# from tkinter.filedialog import *
-#
-# video = askopenfile()
-#
-# vdo = moviepy.editor.VideoClip
-#
-audio = clip.audio
-#
-audio.write_audiofile("audio.mp3")
+
+def browse_file():
+    global file
+    file = filedialog.askopenfilename(initialdir=os.getcwd(), title="select file",filetype=(("Video Files", ".mp4"), ("All Files", "*.*")))
+
+def get_mp4():
+    clip = VideoFileClip(file)
+    audio = clip.audio
+    audio.write_audiofile("audio.mp3")
+
+
+Button(main, text="SELECT VIDEO", command=browse_file, width=15, font="arial 20", bg="orange", bd=4).place(x=40, y=100)
+Button(main, text="CONVERT AUDIO", command=get_mp4, width=15, font="arial 20", bg="red",  bd=4).place(x=330, y=100)
+
+
+main.mainloop()
+
+#*.WEBM, *.MPG, *.MP2,*.MPEG, *.MPE, *.MPV, *.OGG,*.MP4,*.M4P, *.M4V, *.AVI, *.WMV,*.MOV, *.QT,*.FLV, *.SWF,
